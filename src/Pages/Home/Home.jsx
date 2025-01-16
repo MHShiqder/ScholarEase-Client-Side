50e2a87ca2e38de6e14d9852cdf14f800b45fa7f
Hasibul Hasan <hasibul277127@gmail.com>
2025-01-16 15:30:25 +0600

    added the home page

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Slider2 from '../../Component/Slider/Slider2';
+
+const Home = () => {
+    return (
+        <div>
+            
+            <Slider2></Slider2>
+        </div>
+    );
+};
+
+export default Home;
