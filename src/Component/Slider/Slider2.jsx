0ca0defb795005c938cb9d4c7b70477db32e1000
Hasibul Hasan <hasibul277127@gmail.com>
2025-01-16 15:34:01 +0600

    added the slider on the homepage

---
+++ src/Component/Slider/Slider2.jsx
@@ -0,0 +1,45 @@
+import banner1 from '../../assets/banner1.png'
+import banner2 from '../../assets/banner2.png'
+import banner3 from '../../assets/banner3.png'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import './styles.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+
+const Slider2 = () => {
+    return (
+        <div>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: true,
+                }}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img className='w-full' alt="" src={banner1} /></SwiperSlide>
+                <SwiperSlide><img className='w-full' alt="" src={banner3} /></SwiperSlide>
+                <SwiperSlide><img className='w-full' alt="" src={banner2} /></SwiperSlide>
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider2;
