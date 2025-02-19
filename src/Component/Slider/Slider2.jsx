05ca367d7f1c8d269b90ea821958af7a898dbdd7
Hasibul Hasan <hasibul277127@gmail.com>
2025-02-19 15:51:05 +0600

    modified the hero section

--- src/Component/Slider/Slider2.jsx
+++ src/Component/Slider/Slider2.jsx
@@ -1,6 +1,7 @@
 import banner2 from "../../assets/banner2.jpg"
 import banner1 from "../../assets/banner1.jpg"
 import banner3 from "../../assets/banner3.jpg"
+import banner4 from "../../assets/banner4.jpg"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -8,9 +9,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 // Import required Swiper modules
-import { Autoplay, Navigation, EffectFade } from 'swiper/modules';
+import { Autoplay, Navigation, EffectFade,Pagination } from 'swiper/modules';
 import Slide from './Slide';
 
 const Slider2 = () => {
@@ -18,43 +20,56 @@ const Slider2 = () => {
         <div>
             <Swiper
                 spaceBetween={0}
+                speed={1500}
                 effect="fade"
-                fadeEffect={{ crossFade: true }}
+                fadeEffect={{ crossFade: false, }}
                 centeredSlides={true}
                 autoplay={{
                     delay: 3000,
-                    disableOnInteraction: false,
+                    disableOnInteraction: true,
                 }}
                 loop={true}
-                navigation={true}
-                modules={[Autoplay, Navigation, EffectFade]}
+                // navigation={true}
+                pagination={{
+                    clickable:true,
+                }}
+                modules={[Autoplay, Navigation, EffectFade,Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide key={1}>
-                    <Slide key={1}
-                        index={"1"}
+                <SwiperSlide >
+                    <Slide 
+                        
                         Heading="Unlock Your"
                         image={banner2}
                         Bold="Bright Future"
                         subHeading="Education is the key to success, and the right scholarship can unlock limitless opportunities. Start your journey today and secure the future you deserve."
                     ></Slide>
                 </SwiperSlide>
-                <SwiperSlide key={2}>
-                    <Slide key={2}
-                        index={"2"}
+                <SwiperSlide >
+                    <Slide 
+                        
                         Heading="Education Without"
                         image={banner1}
                         Bold="Limits Ahead"
                         subHeading="Financial barriers shouldn’t hold you back from achieving greatness. Explore a world of scholarships designed to support your dreams, no matter where you come from."
                     ></Slide>
                 </SwiperSlide>
-                <SwiperSlide key={3}>
-                    <Slide key={3}
-                        index={"3"}
+                <SwiperSlide >
+                    <Slide 
+                        
                         Heading="Dream Big"
                         image={banner3}
                         Bold="Fund Secure"
                         subHeading="Turn your academic aspirations into reality with the right financial support. Discover grants and scholarships that make higher education stress-free and achievable."
+                    ></Slide>
+                </SwiperSlide>
+                <SwiperSlide >
+                    <Slide 
+                        
+                        Heading="Your Scholarship"
+                        image={banner4}
+                        Bold="Your Success"
+                        subHeading="Finding the right scholarship shouldn’t be complicated. ScholarEase connects you to funding opportunities tailored to your academic goals, making success more accessible."
                     ></Slide>
                 </SwiperSlide>
                 {/* <SwiperSlide><img className="w-full" alt="Slide 2" src={banner3} /></SwiperSlide>
