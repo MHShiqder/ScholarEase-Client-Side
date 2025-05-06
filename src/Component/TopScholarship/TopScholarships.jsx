9d14d10c7c2c598763f43ad42d2d961334a076a5
Hasibul Hasan <hasibul277127@gmail.com>
2025-05-07 01:02:38 +0600

    modified top scholarship tittle

--- src/Component/TopScholarship/TopScholarships.jsx
+++ src/Component/TopScholarship/TopScholarships.jsx
@@ -7,36 +7,41 @@ import loadingImage from '../../assets/navLogoNew2.png'
 
 const TopScholarships = () => {
     const [scholarship, refetch, isLoading] = useHomeScholarship()
-    const [loaded,setLoaded]=useState([])
-    useEffect(()=>{
-        if(scholarship.length>0){
+    const [loaded, setLoaded] = useState([])
+    useEffect(() => {
+        if (scholarship.length > 0) {
             setLoaded(scholarship)
         }
-    },[scholarship])
-   
+    }, [scholarship])
+
     return (
         <div className='text-center w-11/12 mx-auto bg-gradient-to-br from-indigo-50 via-white to-purple-100 px-5 py-10 mt-20'>
-            
+
             {
                 isLoading
-                ?
-                <>
+                    ?
+                    <>
                         <div className=' flex justify-center items-center h-[calc(100vh-100px)]'>
-                                                    {/* <progress className="progress md:w-80 w-48 "></progress> */}
-                                                    <img src={loadingImage} alt="Loading" className='animate-pulse duration-200 w-[300px]' />
-                                                </div>
+                            {/* <progress className="progress md:w-80 w-48 "></progress> */}
+                            <img src={loadingImage} alt="Loading" className='animate-pulse duration-200 w-[300px]' />
+                        </div>
                     </>
                     :
                     <>
-                    <h2 className='text-3xl text-[#320a4e] md:text-4xl bg-[#2E5077 ] font-bold font-lora mb-10'>Top Scholarships</h2>
+                        <div className="text-center">
+                            <h2 className="text-4xl font-extrabold mb-6 text-gray-800">Top Scholarships</h2>
+                            <p className="text-gray-600 mb-12 max-w-3xl mx-auto">
+                            Explore our top scholarship opportunities with approaching deadlines. Don't miss your chance to apply!
+                            </p>
+                        </div>
                         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10'>
                             {
                                 loaded.map((item, idx) => <ScholarshipCard key={idx} item={item} ></ScholarshipCard>)
                             }
                         </div>
                         <Link to='/all-scholarship'><button className='btn rounded-full bg-gradient-to-br from-primary to-indigo-700  transition-all duration-300 hover:scale-105   text-white px-20 '>All Scholarship <span><FaArrowRight></FaArrowRight></span></button></Link>
-                        
-                        
+
+
                     </>
             }
         </div>
